a3089253f9bd080e6457dd17ee5259cdc8fd6e86
Fergus White <ferguswhite@outlook.com>
2021-11-01 17:19:24 +0000

    Update profile.js

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,3 +1,3 @@
 export default function Profile(){
-    return <p> I know the profile doesnt work, im working on that, I have only spent like a week on this chill</p>
+    return <p> I know the profile doesnt work, coming soon</p>
 }
