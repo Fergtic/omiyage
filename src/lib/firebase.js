1674e1ef45b3088c80af25caf1c745d146f8a2ff
Fergus White <ferguswhite@outlook.com>
2023-03-22 16:31:35 +0000

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,16 +4,6 @@ import {getStorage} from 'firebase/storage'
 
 
 
-
-const config = {
-    apiKey: "AIzaSyDeJoqdX5iIiUCTWAyHDHOgUiJ1sUvh2ww",
-    authDomain: "anon2-403d0.firebaseapp.com",
-    projectId: "anon2-403d0",
-    storageBucket: "anon2-403d0.appspot.com",
-    messagingSenderId: "1073586946573",
-    appId: "1:1073586946573:web:215119d0b857700b264b44"
-};
-
 const app = initializeApp(config);
 const FieldValue  = getFirestore();
 const storage = getStorage(app);
